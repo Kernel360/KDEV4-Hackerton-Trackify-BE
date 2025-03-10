9b36608d619e5d2865a7ba926224d56d3bcf65e1
Siyun Lee <isiyun@Siyuns-MacBook-Pro.local>
2025-03-10 17:52:54 +0900

    test: 프론트, 백 연동 테스트

---
+++ src/main/java/com/example/trackify/TestController.java
@@ -0,0 +1,22 @@
+package com.example.trackify;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class TestController {
+
+    @PostMapping("/test")
+    @ResponseBody
+    public Map<String, String> test(
+            @RequestBody Map<String, String> requestData
+    ) {
+        Map<String, String> response = new HashMap<>();
+        response.put("content", "데이터 수신 완료");
+        System.out.println(response);
+        return response;
+    }
+}
