92070f3ba88b44dce25ebd25cdddf758b539b6ab
Siyun Lee <isiyun@Siyuns-MacBook-Pro.local>
2025-03-10 18:26:28 +0900

    feat: Task 엔티티 구현

---
+++ src/main/java/com/example/trackify/task/domain/Task.java
@@ -0,0 +1,42 @@
+package com.example.trackify.task.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Task {
+    @Id @GeneratedValue
+    @Column(name = "task_sequence")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "task_title")
+    private String title;
+
+    @Column(name = "task_start_date")
+    private LocalDate startDate;
+
+    @Column(name = "task_end_date")
+    private LocalDate endDate;
+
+    @Column(name = "task_status")
+    private Integer status;
+
+    //==Task 생성 메서드==//
+    public static Task createTask(String title, LocalDate start_date, LocalDate end_date) {
+        Task task = new Task();
+
+        task.setTitle(title);
+        task.setStartDate(start_date);
+        task.setEndDate(end_date);
+
+        return task;
+    }
+}
