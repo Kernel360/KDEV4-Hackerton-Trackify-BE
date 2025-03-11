1e55e96f6d7edcf05fe814b258ada834c9ae9fdf
jaekyung <rkj0103@naver.com>
2025-03-12 04:08:53 +0900

    FIX: WebConfig.java addInterceptor test

--- src/main/java/com/example/trackify/config/WebConfig.java
+++ src/main/java/com/example/trackify/config/WebConfig.java
@@ -11,13 +11,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Autowired
     private AuthInterceptor authInterceptor;
 
-    @Override
-    public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(authInterceptor)
-                .addPathPatterns("/api/**")   // 보호할 URL 패턴
-                .addPathPatterns("/task/**")
-                .excludePathPatterns("/", "/login", "/signup");  // 예외 (로그인, 회원가입 페이지는 제외)
-    }
+//    @Override
+//    public void addInterceptors(InterceptorRegistry registry) {
+//        registry.addInterceptor(authInterceptor)
+//                .addPathPatterns("/api/**")   // 보호할 URL 패턴
+//                .addPathPatterns("/task/**")
+//                .excludePathPatterns("/", "/login", "/signup");  // 예외 (로그인, 회원가입 페이지는 제외)
+//    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
